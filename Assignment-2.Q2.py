b664846cf9ccaa3173525dfbaeb4c7b544b4338e
AnudeepPanakanti <103166189+AnudeepPanakanti@users.noreply.github.com>
2023-04-15 10:55:03 +0530

    Add files via upload

---
+++ Assignment-2.Q2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+my_dict = {}
+for i in range(97,123):
+    my_dict[chr(i)] = i
+print(my_dict)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
