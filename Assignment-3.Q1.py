5b8fc14fe0722784e77831482e10187839e1bc92
AnudeepPanakanti <103166189+AnudeepPanakanti@users.noreply.github.com>
2023-04-16 20:24:59 +0530

    Add files via upload

---
+++ Assignment-3.Q1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+def sum(numbers):
+    total = 0
+    for x in numbers:
+        total = total+x
+    return total
+num=eval(input("Enter the numbers : "))
+print(sum(num))
+
+
+# In[ ]:
+
+
+
+
