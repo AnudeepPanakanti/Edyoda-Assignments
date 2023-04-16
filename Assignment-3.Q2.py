ff57b9e11429a022473573bd5cae2a0bb66c1ab9
AnudeepPanakanti <103166189+AnudeepPanakanti@users.noreply.github.com>
2023-04-16 20:36:46 +0530

    Add files via upload

---
+++ Assignment-3.Q2.py
@@ -0,0 +1,19 @@
+# In[2]:
+
+def reverse(s):
+    str = ""
+    for i in s:
+        str = i + str
+    return str
+s = input("Enter the string : ")
+print("The original string is : ", end="")
+print(s) 
+print("The reversed string(using loops) is : ", end="")
+print(reverse(s))
+
+
+# In[ ]:
+
+
+
+
